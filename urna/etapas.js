3e9cdd56cc9764389651b0916ade7e5aa62ded52
Luan <luam10@hotmail.com.br>
2022-06-01 23:11:39 -0300

    conclusão do projeto de urna html  css e javaScrip

---
+++ urna/etapas.js
@@ -0,0 +1,74 @@
+let etapas = [{
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [{
+            numero: '38196',
+            nome: 'Fulano de tal',
+            partido: 'nnnn',
+            fotos: [{
+                url: 'images.jpg',
+                legenda: 'Vereador',
+            }]
+
+        }, {
+            numero: '38198',
+            nome: ' de tal Fulano',
+            partido: 'dddd',
+            fotos: [{
+                url: '',
+                legenda: 'Vereador',
+            }]
+
+        }, ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [{
+            numero: '81',
+            nome: 'Fulano de tal',
+            partido: 'nnnn',
+            fotos: [{
+                    url: '',
+                    legenda: 'Prefeito'
+                },
+                {
+                    url: '',
+                    legenda: 'Vice-Prefeito',
+                    small: true,
+                }
+            ]
+
+        }, {
+            numero: '38',
+            nome: 'Fulano de tal',
+            partido: 'dddd',
+            fotos: [{
+                    url: '',
+                    legenda: 'Prefeito'
+                },
+                {
+                    url: '',
+                    legenda: 'Vice-Prefeito',
+                    small: true,
+                }
+            ]
+
+        }, {
+            numero: '96',
+            nome: 'Fulano de tal',
+            partido: 'qqqqq',
+            fotos: [{
+                    url: '',
+                    legenda: 'Prefeito'
+                },
+                {
+                    url: '',
+                    legenda: 'Vice-Prefeito',
+                    small: true,
+                }
+            ]
+
+        }, ]
+    }
+]
